73a5e53226bbe6cfca527fd0156264758b36b09e
tej <tejasreed73@gmail.com>
2026-01-25 18:56:42 +0530

    user repository interface

---
+++ MyTurn/src/main/java/com/myturn/MyTurn/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.myturn.MyTurn.repository;
+import com.myturn.MyTurn.model.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+
+    //avoid duplicates
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+}
