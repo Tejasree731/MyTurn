9f70308a612a7c80aafe53bf54c9918d619dfab5
tej <tejasreed73@gmail.com>
2026-01-27 15:13:46 +0530

    added some more methods

--- MyTurn/src/main/java/com/myturn/MyTurn/repository/QueueTokenRepository.java
+++ MyTurn/src/main/java/com/myturn/MyTurn/repository/QueueTokenRepository.java
@@ -2,10 +2,20 @@ package com.myturn.MyTurn.repository;
 
 import com.myturn.MyTurn.model.QueueToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface QueueTokenRepository extends JpaRepository<QueueToken, Long> {
 
     List<QueueToken> findByQueueIdAndStatusOrderByTokenNumber(
             Long queueId, String status);
+    QueueToken findByQueueIdAndUsernameAndStatus(
+        Long queueId, String username, String status);
+
+    @Query("SELECT MAX(t.tokenNumber) FROM QueueToken t WHERE t.queue.id = :queueId")
+    Integer findMaxToken(@Param("queueId") Long queueId);
+
+
 }
