a4ff5269ba964c55c9d54e9a8459258902a918da
tej <tejasreed73@gmail.com>
2026-01-27 22:23:46 +0530

    login only using jwt

--- MyTurn/src/main/java/com/myturn/MyTurn/security/SecurityConfig.java
+++ MyTurn/src/main/java/com/myturn/MyTurn/security/SecurityConfig.java
@@ -4,18 +4,34 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http,
+                                           JwtFilter jwtFilter)
+            throws Exception {
 
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()
-            );
+
+                //Public
+                .requestMatchers("/api/auth/**",
+                                 "/login.html",
+                                 "/dashboard.html",
+                                 "/queue.html",
+                                 "/ws/**")
+                .permitAll()
+
+                //Protected
+                .anyRequest().authenticated()
+            )
+
+            .addFilterBefore(jwtFilter,
+                UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
